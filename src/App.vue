fc35a037c7fb6e74a6cc8c852b55ba7a80da37fc
vijaykumar <vijay1111mane@gmail.com>
2022-05-27 16:54:09 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <pre>
+    {{ JSON.stringify(formValues, null, 2) }}
+  </pre
+    >
+  </div>
+  <form @submit="submitForm">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model="formValues.name" />
+    </div>
+    <br />
+    <div>
+      <label for="profile">Profile Summary</label>
+      <textarea id="profile" v-model="formValues.profileSummary" />
+    </div>
+    <br />
+    <div>
+      <label for="country">Country</label>
+      <select id="country" v-model="formValues.country">
+        <option value="">Select a country</option>
+        <option value="india">India</option>
+        <option value="America">America</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+    <br />
+    <div>
+      <label for="job-location">Job Location</label>
+      <select id="job-location" multiple v-model="formValues.jobLocation">
+        <option value="india">India</option>
+        <option value="America">America</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+    <br />
+    <hr />
+    <br />
+    <div>
+      <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" true-value="yes" false-value="no" />
+      <label for="remoteWork">Open to remote work?</label>
+    </div>
+    <br />
+    <div>
+      <label>Skill set</label>
+      <input type="checkbox" id="html" value="html" v-model="formValues.skillSet" />
+      <label for="html">HTML</label>
+      <input type="checkbox" id="css" value="css"
+        v-model="formValues.skillSet" />
+      <label for="css">CSS</label>
+      <input type="checkbox" id="javascript" value="javascript"
+       v-model="formValues.skillSet" />
+      <label for="javascript">JavaScript</label>
+    </div>
+    <br />
+    <div>
+      <label>Years of Experience</label>
+      <input type="radio" id="0-2" value="0-2"
+        v-model="formValues.yearsOfExperience"  />
+
+      <label for="0-2">0-2</label>
+      <input type="radio" id="3-5" value="3-5"
+        v-model="formValues.yearsOfExperience" />
+
+      <label for="3-5">3-5</label>
+      <input type="radio" id="6-10" value="6-10"
+        v-model="formValues.yearsOfExperience" />
+
+      <label for="5-10">5-10</label>
+      <input type="radio" id="10+" value="10+"
+       v-model="formValues.yearsOfExperience" />
+
+      <label for="10+">10+</label>
+    </div>
+    <div>
+      <button>Submit</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      formValues: {
+        name: "",
+        profileSummary: "",
+        country: "",
+        jobLocation: [],
+        remoteWork: "no",
+        skillSet: [],
+        yearsOfExperience: "",
+      },
+    };
+  },
+  methods: {
+    submitForm(event) {
+      event.preventDefault();
+      console.log("Form values", this.formValues);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+input[type="text"],
+textarea,
+select {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
